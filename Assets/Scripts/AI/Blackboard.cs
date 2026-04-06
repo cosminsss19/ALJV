236ae86519fc39f0eccb1260f71d471e38055474
cosminsss19 <cosminsss.alex19@gmail.com>
2026-04-06 22:46:28 +0300

    Useless lines deleted. If it ruins stuff, revert

--- Assets/Scripts/AI/Blackboard.cs
+++ Assets/Scripts/AI/Blackboard.cs
@@ -8,8 +8,6 @@ namespace AI
         public GameObject Player;
         public Vector3 TargetPosition;
         public List<Vector3> CurrentPath = new List<Vector3>();
-        public float LastAlertTime = -Mathf.Infinity;
-        public bool Alerted = false;
 
         private void Awake()
         {
